6e4cb9e3955372e1acd937d3c4b889b95930913d
ykolarov <kolarov01@protonmail.me>
2022-07-01 18:54:39 +0100

    Fetch pledge, active stake, and blocks minted

--- src/main/java/me/yanaki/adatest/Main.java
+++ src/main/java/me/yanaki/adatest/Main.java
@@ -1,11 +1,13 @@
 package me.yanaki.adatest;
 
-import io.blockfrost.sdk.api.BlockService;
+import io.blockfrost.sdk.api.EpochService;
 import io.blockfrost.sdk.api.PoolService;
 import io.blockfrost.sdk.api.exception.APIException;
-import io.blockfrost.sdk.api.model.Block;
+import io.blockfrost.sdk.api.model.Epoch;
+import io.blockfrost.sdk.api.model.Pool;
+import io.blockfrost.sdk.api.model.PoolMetadata;
 import io.blockfrost.sdk.api.util.Constants;
-import io.blockfrost.sdk.impl.BlockServiceImpl;
+import io.blockfrost.sdk.impl.EpochServiceImpl;
 import io.blockfrost.sdk.impl.PoolServiceImpl;
 
 import java.util.List;
@@ -20,26 +22,27 @@ public class Main {
     private static void printPoolData() {
         PoolService poolService = new PoolServiceImpl(
                 Constants.BLOCKFROST_TESTNET_URL, System.getenv("PROJECT_ID"));
-        try {
-            List<String> allPools = poolService.getAllPools();
-            for (String pool: allPools) {
-                System.out.println(pool + poolService.getPoolMetadata(pool));
-            }
-
-
-        } catch (APIException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    private static void printLastBlock() {
-        BlockService blockService = new BlockServiceImpl(
+        EpochService epochService = new EpochServiceImpl(
                 Constants.BLOCKFROST_TESTNET_URL, System.getenv("PROJECT_ID"));
         try {
-            Block block = blockService.getLatestBlock();
-            System.out.println(block.toString());
+            List<String> allPools = poolService.getAllPools();
+            for (String poolString: allPools) {
+                Pool pool = poolService.getPool(poolString);
+                PoolMetadata poolMetadata = poolService.getPoolMetadata(poolString);
+
+                String poolName = poolMetadata.getName() == null ? "Unnamed" : poolMetadata.getName();
+
+                String livePledge = pool.getLivePledge();
+                String activeStake = pool.getActiveStake();
+                Integer blocksMinted = pool.getBlocksMinted();
+
+                System.out.println(poolName + " has a live pledge of: " +
+                        livePledge + " and an active stake of: " + activeStake +
+                        " and has minted " + blocksMinted + " blocks");
+            }
         } catch (APIException e) {
             throw new RuntimeException(e);
         }
     }
+
 }
